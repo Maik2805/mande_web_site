2b588bd417e2aa5a7b8f41a8f950d36fbd24831a
Maik2805 <mscardenas77@misena.edu.co>
2023-01-14 16:54:03 -0500

    Update

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,26 +1,21 @@
 $(window).on("load", function () {
     const userData = getUserData();
     if (!userData) {
+        alert("Debe iniciar sesión");
         window.location.replace("./login.html");
     } else if (new Date(userData.exp * 1000) < new Date()) {
         console.log(new Date(userData.exp));
         alert("Sesión expirada");
         window.location.replace("./login.html");
-    } else {
-        alert("OK");
     }
 });
 
 $(document).ready(function () {
-    alert("after")
-    //     const userData = getUserData();
-    //     if (!userData) {
-    //         window.location.replace("./login.html");
-    //     } else if (new Date(userData.exp * 1000) < new Date()) {
-    //         console.log(new Date(userData.exp));
-    //         alert("Sesión expirada");
-    //         window.location.replace("./login.html");
-    //     } else {
-    //         alert("OK");
-    //     }
+    console.log("Page Ready")
+    completeUserInfo();
 });
+
+function completeUserInfo() {
+    const userData = getUserData().usuario;
+    $("#username_header").text(userData.nombre_completo)
+}
