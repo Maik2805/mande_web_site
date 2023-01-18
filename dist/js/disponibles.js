b8cb378909478f83afb1dc9232b3f33e41a20a3a
Maik2805 <mscardenas77@misena.edu.co>
2023-01-17 23:11:52 -0500

    Avance carga info trabajador a contratar

--- dist/js/disponibles.js
+++ dist/js/disponibles.js
@@ -1,12 +1,10 @@
 $(document).ready(function () {
     getUsuario()
         .then((usuario) => {
-            console.log("aaaaaa", usuario)
             if (!usuario.fotoRecibo) {
                 alert("Debe Cargar una foto del recibo para contratar servicios");
             } else {
                 var texto = usuario.fotoRecibo.split("_");
-                console.log(texto)
                 $("#lbl_imagen").text(texto[1] ? texto[1] : usuario.fotoRecibo);
             }
         }).catch((err) => console.error(err));
@@ -81,7 +79,7 @@ $("#cargar_recibo").on("submit", function (event) {
 function ajaxRequest(params) {
     console.log("aquiii");
     $.ajax({
-        url: BASE_URL + "labores/disponibles/",
+        url: BASE_URL + "labores/disponiblesRaw",
         success: (data) => {
             console.log(data);
             params.success({
@@ -91,5 +89,52 @@ function ajaxRequest(params) {
         dataType: "json",
         headers: { "Authorization": "Bearer " + getToken() }
     });
+
+}
+
+function contratarFormatter(value, row) {
+    let element = `<button
+    type="button" class="btn btn-outline-info btn-sm"
+    data-toggle="modal" data-target="#trabajadorModal" data-user="${value}">Contratar</button>`;
+    return element;
+}
+
+$('#trabajadorModal').on('show.bs.modal', function (event) {
+    $("#modalBody").html(`<div class="text-center">
+                    <div class="spinner-border text-secondary m-4" role="status">
+                    <span class="sr-only">Loading...</span>
+                    </div>
+                </div>`);
+    var button = $(event.relatedTarget);
+    var usuario = button.data('user');
+    $.ajax({
+        url: BASE_URL + "usuarios/getPublicInfo/" + usuario,
+        success: (data) => loadTrabajadorCard(data),
+        error: (msg) => {
+            $(this).modal('hide');
+            alert("Error: " + msg.responseText)
+        },
+        dataType: "json",
+        headers: { "Authorization": "Bearer " + getToken() }
+    });
+    var modal = $(this)
+    // modal.find('#modal_content').text(usuario)
+    // modal.find('.modal-body input').val(recipient)
+});
+
+function loadTrabajadorCard(data) {
+    console.log(data);
+    let html = `<div class="card" style="width:400px">
+    <img class="card-img-top" src="${BASE_URL}public/data_lake/imagenes/${data.fotoPerfil}"
+        alt="Card image">
+    <div class="card-body">
+        <h4 class="card-title">${data.nombreCompleto}</h4>
+        <h5 class="card-title">Email: ${data.correoElectronico}</h5>
+        <h6>Numero Estrellas: ${data.numeroEstrellas}</h6>
+        <h6>Numero Estrellas: ${data.promedioEstrellas}</h6>
+        <a href="#" class="btn btn-primary text-center">Contratar</a>
+    </div>
+</div>`;
+    $("#modalBody").html(html);
 
 }
