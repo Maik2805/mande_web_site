43f9aded35daebd5e0ab97fec9d501cd598166e7
CAMILO\Usuario <gomezcamilo9701@gmail.com>
2023-01-28 16:48:33 -0500

    init de métodos de pago, se deja la opcion del navbar comentada

---
+++ dist/js/ingresaPago.js
@@ -0,0 +1,12 @@
+$(window).on("load", function () {
+  const userData = getUserData();
+  if (!userData) {
+    alert("Debe iniciar sesión");
+    window.location.replace("./login.html");
+  } else if (new Date(userData.exp * 1000) < new Date()) {
+    console.log(new Date(userData.exp));
+    alert("Sesión expirada");
+    window.location.replace("./login.html");
+  }
+});
+
